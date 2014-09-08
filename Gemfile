9c6869d860f7aee258da82b8bb74b0235f45d4c1
David <dajacobs@memphis.edu>
2014-09-08 12:50:50 -0500

    Initialize repository

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '4.0.8'
 
 group :development, :test do
 	gem 'sqlite3', '1.3.8'
-	gem 'rspec-rails', '2.13.1'	
+	gem 'rspec-rails', '2.13.1'
 end
 
 group :test do
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,11 +10,10 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 require 'securerandom'
-
 def secure_token
 	token_file = Rails.root.join('.secret')
 	if File.exist?(token_file)
-		#Use the existing token.
+		# Use the existing token.
 		File.read(token_file).chomp
 	else
 		# Generate a new token and store it in token_file.
@@ -23,5 +22,4 @@ def secure_token
 		token
 	end
 end
-
 SampleApp::Application.config.secret_key_base = secure_token
