12b38ffeb8c47b459dada2a1e621e991a332d3a1
David <dajacobs@memphis.edu>
2014-09-09 19:56:03 -0500

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,25 +5,25 @@ describe "Static pages" do
 	describe "Home page" do
 
 		it "should ahve the content 'Sample App'" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).to have_content('Sample App')
 		end
 
-		it "should have the title 'Home'" do
-			visit '/static_pages/home'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+		it "should have the base title" do
+			visit root_path
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App")
 		end							
 	end
 
 	describe "Help page" do
 
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_content('Help')
 		end
 
 		it "should have the title 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
 		end	
 	end
@@ -31,13 +31,27 @@ describe "Static pages" do
 	describe "About page" do
 
 		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_content('About Us')
 		end
 		
 		it "should have the title 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
-		end	
+		end
+	end
+
+
+	describe "Contact page" do
+
+		it "should have the content 'Contact'" do
+			visit contact_path
+			expect(page).to have_content('Contact')
+		end
+		
+		it "should have the tile 'Contact'" do
+			visit contact_path
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+		end
 	end
 end
